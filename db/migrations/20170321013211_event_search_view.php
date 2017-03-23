b94593bb2180ba89bb009a09ebc9d597211150f8
luizfsnunes <luizfsnunes@gmail.com>
2017-03-23 20:51:59 -0300

    Atualizada view para levar em consideração a tags.

--- db/migrations/20170321013211_event_search_view.php
+++ db/migrations/20170321013211_event_search_view.php
@@ -7,16 +7,21 @@ class EventSearchView extends AbstractMigration
     public function up()
     {
         $sql = "DROP VIEW IF EXISTS event_search;
-            CREATE VIEW event_search AS
-            SELECT e.id, u.id as user_id, name, title,  date(eventdate) as eventdate, group_concat(c.description) as categories
-            FROM users as u
-                  INNER JOIN events as e
-                    ON u.id = e.user_id
-                  INNER JOIN event_categories as ec
-                    ON e.id = ec.event_id
-                  INNER JOIN categories as c
-                    ON c.id = ec.category_id	    
-            GROUP BY e.id;";
+                CREATE VIEW event_search AS
+                SELECT e.id, u.id as user_id, name, title,  date(eventdate) as eventdate,
+                         group_concat(c.description) as categories, group_concat(t.description) as tags
+                FROM users as u
+                      INNER JOIN events as e
+                        ON u.id = e.user_id
+                      INNER JOIN event_categories as ec
+                        ON e.id = ec.event_id
+                      INNER JOIN categories as c
+                        ON c.id = ec.category_id	    
+                      INNER JOIN event_tags as et
+                        ON e.id = et.event_id
+                      INNER JOIN tags as t
+                        ON t.id = et.tag_id	 
+                GROUP BY e.id;;
 
         $this->execute($sql);
     }
