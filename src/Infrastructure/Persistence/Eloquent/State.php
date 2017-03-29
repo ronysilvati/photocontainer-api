d2b1ae6b160c65906e4518887ca40ec4dc955ef8
luizfsnunes <luizfsnunes@gmail.com>
2017-03-29 02:48:05 -0300

    Modelos para as tabelas de cidade/estado.

---
+++ src/Infrastructure/Persistence/Eloquent/State.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PhotoContainer\PhotoContainer\Infrastructure\Persistence\Eloquent;
+
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+
+class State extends EloquentModel
+{
+    protected $table = 'states';
+}
