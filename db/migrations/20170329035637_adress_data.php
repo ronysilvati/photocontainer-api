c966f81aa5ddcced26d2505c1408f1efd23cbb30
luizfsnunes <luizfsnunes@gmail.com>
2017-03-29 02:45:28 -0300

    Adiciona tabelas com cidades/estados do Brasil.

---
+++ db/migrations/20170329035637_adress_data.php
@@ -0,0 +1,30 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AdressData extends AbstractMigration
+{
+
+    public function change()
+    {
+        $table = $this->table('countries');
+        $table->addColumn('name', 'string', ['limit' => 60])
+            ->addColumn('countrycode', 'string', ['limit' => 10])
+            ->addIndex('countrycode')
+            ->create();
+
+        $table = $this->table('states');
+        $table->addColumn('country_id', 'integer')
+            ->addForeignKey('country_id', 'countries', 'id', ['delete'=> 'RESTRICT', 'update'=> 'NO_ACTION'])
+            ->addColumn('name', 'string', ['limit' => 60])
+            ->addColumn('statecode', 'string', ['limit' => 10])
+            ->addIndex('statecode')
+            ->create();
+
+        $table = $this->table('cities');
+        $table->addColumn('state_id', 'integer')
+            ->addForeignKey('state_id', 'states', 'id', ['delete'=> 'RESTRICT', 'update'=> 'NO_ACTION'])
+            ->addColumn('name', 'string', ['limit' => 60])
+            ->create();
+    }
+}
