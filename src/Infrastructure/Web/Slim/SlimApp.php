c191df0c9fc1e21aa0f4ad8f9ab73102345ed853
Leonardo Cidral <leonardo@saochico.com>
2017-04-08 18:53:53 -0300

    Adicionado o Cache-Control

--- src/Infrastructure/Web/Slim/SlimApp.php
+++ src/Infrastructure/Web/Slim/SlimApp.php
@@ -38,7 +38,7 @@ class SlimApp implements WebApp
             $response = $next($req, $res);
             return $response
                 ->withHeader('Access-Control-Allow-Origin', '*')
-                ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+                ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization, Cache-Control')
                 ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
         });
     }
