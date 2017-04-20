1d031a01086bdd8ddd7a406d2720ca274c03ee60
Leonardo Cidral <leonardo@saochico.com>
2017-04-19 23:55:50 -0300

    Movida a validação para dentro do laço e alterado o operador para maior ou igual

--- src/Contexts/Photo/Action/CreatePhoto.php
+++ src/Contexts/Photo/Action/CreatePhoto.php
@@ -25,18 +25,19 @@ class CreatePhoto
     public function handle(array $array, int $event_id)
     {
         try {
-            $eventPhotos = $this->dbRepo->findEventPhotos($event_id);
-
-            if (count($eventPhotos) > 30) {
-                throw new \Exception('O limite de fotos foi atingido.');
-            }
-
             foreach ($array as $item) {
                 try {
+
+                    $eventPhotos = $this->dbRepo->findEventPhotos($event_id);
+                    if (count($eventPhotos) >= 30) {
+                        throw new \Exception('O limite de fotos foi atingido.');
+                    }
+
                     $this->fsRepo->create($item);
                     $this->dbRepo->create($item);
                 } catch (\Exception $e) {
                     $this->fsRepo->rollback($item);
+                    return new DomainExceptionResponse($e->getMessage());
                 }
             }
             return new PhotoResponse($array);
