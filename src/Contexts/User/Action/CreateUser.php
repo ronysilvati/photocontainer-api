337602dc888f4be3c7b637977e921d9d54330ba6
luizfsnunes <luizfsnunes@gmail.com>
2017-05-18 21:59:02 -0300

    Corrigir envio de email

--- src/Contexts/User/Action/CreateUser.php
+++ src/Contexts/User/Action/CreateUser.php
@@ -2,27 +2,74 @@
 
 namespace PhotoContainer\PhotoContainer\Contexts\User\Action;
 
+use PhotoContainer\PhotoContainer\Contexts\User\Domain\Profile;
+use PhotoContainer\PhotoContainer\Contexts\User\Domain\User;
 use PhotoContainer\PhotoContainer\Contexts\User\Domain\UserRepository;
+use PhotoContainer\PhotoContainer\Contexts\User\Email\NewUserEmail;
 use PhotoContainer\PhotoContainer\Contexts\User\Response\UserCreatedResponse;
-use PhotoContainer\PhotoContainer\Infrastructure\Entity;
+use PhotoContainer\PhotoContainer\Infrastructure\Crypto\CryptoMethod;
+use PhotoContainer\PhotoContainer\Infrastructure\Event\EventGeneratorTrait;
 use PhotoContainer\PhotoContainer\Infrastructure\Web\DomainExceptionResponse;
 
 class CreateUser
 {
+    use EventGeneratorTrait;
+
+    /**
+     * @var UserRepository
+     */
     protected $userRepository;
 
-    public function __construct(UserRepository $userRepository)
+    /**
+     * @var CryptoMethod
+     */
+    protected $cryptoMethod;
+
+    /**
+     * CreateUser constructor.
+     * @param UserRepository $userRepository
+     * @param CryptoMethod $cryptoMethod
+     */
+    public function __construct(UserRepository $userRepository, CryptoMethod $cryptoMethod)
     {
         $this->userRepository = $userRepository;
+        $this->cryptoMethod = $cryptoMethod;
     }
 
-    public function handle(Entity $user, string $encryptedPwd)
+    /**
+     * @param User $user
+     * @return UserCreatedResponse|DomainExceptionResponse
+     */
+    public function handle(User $user)
     {
         try {
-            $user = $this->userRepository->createUser($user, $encryptedPwd);
+            $encrypted = empty($user->getPwd()) ? '' : $this->cryptoMethod->hash($user->getPwd());
+            $user = $this->userRepository->createUser($user, $encrypted);
+
+            $this->sendEmail($user);
+
             return new UserCreatedResponse($user);
         } catch (\Exception $e) {
             return new DomainExceptionResponse($e->getMessage());
         }
+    }
+
+    /**
+     * @param User $user
+     */
+    private function sendEmail(User $user)
+    {
+        $data = [
+            '{NAME}' => $user->getName(),
+            '{EMAIL}' => $user->getEmail(),
+            '{PROFILE}' => $user->getProfile()->getProfileId() === Profile::PHOTOGRAPHER ? 'Fotografo' : 'Publisher',
+        ];
+
+        $email = new NewUserEmail(
+            $data,
+            ['name' => getenv('ADMIN_EMAIL_NAME'), 'email' => getenv('ADMIN_EMAIL')]
+        );
+
+        $this->addEvent('generic.sendmail', $email);
     }
 }
