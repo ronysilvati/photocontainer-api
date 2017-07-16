30079aa928d9ddf37f9cec0277676fc3052bd98c
luizfsnunes <luizfsnunes@gmail.com>
2017-07-15 21:14:09 -0300

    Corrigir máscara de data

--- src/Contexts/User/Action/CreateUser.php
+++ src/Contexts/User/Action/CreateUser.php
@@ -80,7 +80,7 @@ class CreateUser
             '{NAME}' => $user->getName(),
             '{EMAIL}' => $user->getEmail(),
             '{PROFILE}' => $user->getProfile()->getProfileId() === Profile::PHOTOGRAPHER ? 'Fotografo' : 'Publisher',
-            '{CREATIONDATE}' => date('D/m/y H:i:s')
+            '{CREATIONDATE}' => date('d/m/y H:i:s')
         ];
 
         $email = new NewUserEmail(
