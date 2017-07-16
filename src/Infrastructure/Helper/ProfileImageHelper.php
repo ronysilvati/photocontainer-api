0019878dcdf067d1f143a0d80fe6eebc6d32cd80
luizfsnunes <luizfsnunes@gmail.com>
2017-07-15 21:13:26 -0300

    Gerenciar o nome da imagem do perfil

---
+++ src/Infrastructure/Helper/ProfileImageHelper.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace PhotoContainer\PhotoContainer\Infrastructure\Helper;
+
+use Ramsey\Uuid\Uuid;
+
+class ProfileImageHelper
+{
+    public function generateName($user_id, $file): string
+    {
+        $extensions = ['image/jpeg' => 'jpg', 'image/png' => 'png'];
+        return $this->generateFilename($user_id).'.'.$extensions[$file['type']];
+    }
+
+    public function resolveUri($user_id): ?string
+    {
+        $list = glob(getenv('SHARED_PATH').'/profile_images/'.$user_id.'-*.*');
+        if (empty($list)) {
+            return null;
+        }
+
+        $parts = explode('/', $list[0]);
+
+        return 'profile_images/'.end($parts).'?_t='.time();
+    }
+
+    private function generateFilename($user_id): string
+    {
+        return $user_id.'-'.Uuid::uuid4()->toString();
+    }
+}
