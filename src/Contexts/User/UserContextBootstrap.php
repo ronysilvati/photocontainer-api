bd82bf33fd0e127375c7fbccee25b4b6732a415e
luizfsnunes <luizfsnunes@gmail.com>
2017-07-15 21:16:11 -0300

    Criar rota de envio de imagem de perfil e atualizar a rota de retorno de dados de usuário

--- src/Contexts/User/UserContextBootstrap.php
+++ src/Contexts/User/UserContextBootstrap.php
@@ -8,12 +8,14 @@ use PhotoContainer\PhotoContainer\Contexts\User\Action\FindUser;
 use PhotoContainer\PhotoContainer\Contexts\User\Action\RequestPwdChange;
 use PhotoContainer\PhotoContainer\Contexts\User\Action\UpdatePassword;
 use PhotoContainer\PhotoContainer\Contexts\User\Action\UpdateUser;
+use PhotoContainer\PhotoContainer\Contexts\User\Action\UploadProfileImage;
 use PhotoContainer\PhotoContainer\Contexts\User\Domain\Details;
 use PhotoContainer\PhotoContainer\Contexts\User\Domain\Profile;
 use PhotoContainer\PhotoContainer\Contexts\User\Domain\User;
 use PhotoContainer\PhotoContainer\Contexts\User\Persistence\EloquentUserRepository;
 use PhotoContainer\PhotoContainer\Infrastructure\ContextBootstrap;
 use PhotoContainer\PhotoContainer\Infrastructure\Exception\DomainViolationException;
+use PhotoContainer\PhotoContainer\Infrastructure\Helper\ImageHelper;
 use PhotoContainer\PhotoContainer\Infrastructure\Helper\TokenGeneratorHelper;
 use PhotoContainer\PhotoContainer\Infrastructure\Web\WebApp;
 use Psr\Http\Message\ResponseInterface;
@@ -31,7 +33,11 @@ class UserContextBootstrap implements ContextBootstrap
             $id = isset($args['id']) ? $args['id'] : null;
             $email = isset($args['email']) ? $args['email'] : null;
 
-            $action = new FindUser(new EloquentUserRepository($container['DatabaseProvider']));
+            $action = new FindUser(
+                new EloquentUserRepository($container['DatabaseProvider']),
+                $container['ProfileImageHelper']
+            );
+
             $actionResponse = $action->handle($id, $email);
 
             return $response->withJson($actionResponse, $actionResponse->getHttpStatus());
@@ -86,6 +92,20 @@ class UserContextBootstrap implements ContextBootstrap
             return $response->withJson($actionResponse, $actionResponse->getHttpStatus());
         });
 
+        $slimApp->app->post('/users/{id}/profileImage', function (ServerRequestInterface $request, ResponseInterface $response, $args) use ($container) {
+            if (!isset($_FILES['profile_image']['error']) || is_array($_FILES['profile_image']['error'])) {
+                return $response->withJson(['message' => 'Erro no recebimento da imagem.'], 500);
+            }
+
+            $action = new UploadProfileImage(
+                new EloquentUserRepository($container['DatabaseProvider']),
+                new ImageHelper(getenv('SHARED_PATH').'/profile_images/'),
+                $container['ProfileImageHelper']
+            );
+            $actionResponse = $action->handle((int) $args['id'], $_FILES['profile_image']);
+
+            return  $response->withJson($actionResponse, $actionResponse->getHttpStatus());
+        });
 
         $slimApp->app->post('/users/requestPasswordChange', function (ServerRequestInterface $request, ResponseInterface $response) use ($container) {
             $data = $request->getParsedBody();
