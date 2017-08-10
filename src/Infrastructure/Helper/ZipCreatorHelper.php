03bf43d64b4a929f42274379baa1a71fe3583acf
luizfsnunes <luizfsnunes@gmail.com>
2017-08-10 00:15:55 -0300

    Criar gerador de zip

---
+++ src/Infrastructure/Helper/ZipCreatorHelper.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace PhotoContainer\PhotoContainer\Infrastructure\Helper;
+
+use \ZipArchive;
+
+class ZipCreatorHelper
+{
+    /**
+     * @param string $name
+     * @param array $filesForZip
+     * @return bool
+     */
+    public function createFromFiles(string $name, array $filesForZip): bool
+    {
+        try {
+            $zip = new ZipArchive();
+            $zip->open($name, ZipArchive::CREATE);
+
+            foreach ($filesForZip as $filepath) {
+                $parts = explode('/', $filepath);
+
+                if (!file_exists($filepath) || !$zip->addFile($filepath, end($parts))) {
+                    throw new \RuntimeException('Could not add file do zip.');
+                }
+            }
+
+            return $zip->close();
+        } catch (\Exception $e) {
+            throw new \RuntimeException($e->getMessage());
+        }
+    }
+}
