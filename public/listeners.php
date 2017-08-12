c0fffa6733b327d85df7532c23b0e5ebce098775
luizfsnunes <luizfsnunes@gmail.com>
2017-08-12 11:33:39 -0300

    Configurar listeners

--- public/listeners.php
+++ public/listeners.php
@@ -3,14 +3,20 @@ use PhotoContainer\PhotoContainer\Application\EventListeners\SendEmailOnPublishe
 use PhotoContainer\PhotoContainer\Application\EventListeners\SendEmailOnUserCreated;
 use PhotoContainer\PhotoContainer\Application\EventListeners\SendEmailPhotographerOnDownload;
 use PhotoContainer\PhotoContainer\Application\EventListeners\SendEmailPublisherOnDownload;
-use PhotoContainer\PhotoContainer\Application\EventListeners\GenerateDownloadLog;
+use PhotoContainer\PhotoContainer\Application\EventListeners\SendEmailOnDownloadRequest;
+use PhotoContainer\PhotoContainer\Application\EventListeners\SendEmailOnRequestResponse;
+
+$container = $app->getContainer();
 
 /** @var \League\Event\Emitter $eventEmitter */
-$eventEmitter = $app->getContainer()->get(\League\Event\Emitter::class);
+$eventEmitter = $container->get(\League\Event\Emitter::class);
 
-$eventEmitter->addListener('user_created', $app->getContainer()->get(SendEmailOnUserCreated::class));
+$eventEmitter->addListener('user_created', $container->get(SendEmailOnUserCreated::class));
 
-$eventEmitter->addListener('publisher_registered', $app->getContainer()->get(SendEmailOnPublisherCreated::class));
+$eventEmitter->addListener('publisher_registered', $container->get(SendEmailOnPublisherCreated::class));
 
-$eventEmitter->addListener('downloaded_photo', $app->getContainer()->get(SendEmailPhotographerOnDownload::class));
-$eventEmitter->addListener('downloaded_photo', $app->getContainer()->get(SendEmailPublisherOnDownload::class));
+$eventEmitter->addListener('downloaded_photo', $container->get(SendEmailPhotographerOnDownload::class));
+$eventEmitter->addListener('downloaded_photo', $container->get(SendEmailPublisherOnDownload::class));
+
+$eventEmitter->addListener('download_requested', $container->get(SendEmailOnDownloadRequest::class));
+$eventEmitter->addListener('download_request_response', $container->get(SendEmailOnRequestResponse::class));
