b88cda1a4913e95815797b78009d8f888750f6dc
luizfsnunes <luizfsnunes@gmail.com>
2017-10-22 21:10:30 -0200

    Transformar em seed, em vez de migração

---
+++ db/seeds/TagsUpdate.php
@@ -0,0 +1,32 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class TagsUpdate extends \Phinx\Seed\AbstractSeed
+{
+
+    public function change()
+    {
+        $data = [
+            [
+                'tag_category_id' => 9,
+                'description' => 'Nenhuma',
+                'created_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'tag_category_id' => 5,
+                'description' => 'Outro',
+                'created_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'tag_category_id' => 1,
+                'description' => 'Outro',
+                'created_at' => date('Y-m-d H:i:s'),
+            ],
+        ];
+        $tags = $this->table('tags');
+        $tags->insert($data)->save();
+
+        $this->execute("UPDATE tags SET description = 'Nenhuma' WHERE id = 49");
+    }
+}
