d661c7116be9c7f47652bad4768a8a4cb68cb8b5
luizfsnunes <luizfsnunes@gmail.com>
2017-10-22 21:11:27 -0200

    Carregar rotas e listeners a partir de YAMLs

--- src/Infrastructure/Web/Slim/SlimApp.php
+++ src/Infrastructure/Web/Slim/SlimApp.php
@@ -2,21 +2,17 @@
 
 namespace PhotoContainer\PhotoContainer\Infrastructure\Web\Slim;
 
-use League\Event\CallbackListener;
-use Monolog\Handler\StreamHandler;
-use Monolog\Logger;
 use PhotoContainer\PhotoContainer\Infrastructure\Cache\CacheHelper;
 use PhotoContainer\PhotoContainer\Infrastructure\Event\Event;
 use PhotoContainer\PhotoContainer\Infrastructure\Event\EventRecorder;
 use PhotoContainer\PhotoContainer\Infrastructure\Event\EventWrapper;
 use PhotoContainer\PhotoContainer\Infrastructure\Persistence\EloquentDatabaseProvider;
 use PhotoContainer\PhotoContainer\Infrastructure\Web\WebApp;
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Log\LoggerInterface;
 use Slim\App;
-use Slim\Exception\MethodNotAllowedException;
-use Slim\Exception\NotFoundException;
 use Slim\Http\Response;
 use Slim\Middleware\JwtAuthentication;
 use Zeuxisoo\Whoops\Provider\Slim\WhoopsMiddleware;
@@ -29,12 +25,18 @@ class SlimApp implements WebApp
     public $app;
 
     /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
      * SlimApp constructor.
      * @param App $app
      */
     public function __construct(App $app)
     {
         $this->app = $app;
+        $this->container = $app->getContainer();
     }
 
     /**
@@ -44,17 +46,47 @@ class SlimApp implements WebApp
      */
     public function bootstrap(array $conf): void
     {
-        $app = $this->app;
-        $container = $app->getContainer();
-        $eventDispatcher = $container->get('EventDispatcher');
+        $cacheHelper = $this->container->get(CacheHelper::class);
+        $this->loadRoutes($cacheHelper);
+        $this->setListeners($cacheHelper);
 
-        $container->get(EloquentDatabaseProvider::class);
+        $this->container->get(EloquentDatabaseProvider::class);
 
-        require_once ROOT_DIR.'/src/Application/Resources/routes.php';
-        require_once ROOT_DIR.'/src/Application/Resources/listeners.php';
+        $this->middlewares($conf);
+        $this->defaultRoutes();
+    }
 
+    public function defaultRoutes(): void
+    {
+        $this->app->options('/{routes:.+}', function (ServerRequestInterface $request, ResponseInterface $response) {
+            return $response;
+        });
+
+        $this->app->add(function (ServerRequestInterface $request, ResponseInterface $response, $next) {
+            /** @var ResponseInterface $response */
+            $response = $next($request, $response);
+            return $response
+                ->withHeader('Access-Control-Allow-Origin', '*')
+                ->withHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, X-Requested-With, Content-Type, Accept, Origin, Authorization, Cache-Control')
+                ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH')
+                ->withHeader('Access-Control-Max-Age', '604800');
+        });
+
+        $this->app->get('/purge', function (ServerRequestInterface $request, ResponseInterface $response) {
+            /** @var CacheHelper $cache */
+            $cache = $this->get(CacheHelper::class);
+            $cache->purge();
+            return new Response(204);
+        });
+    }
+
+    /**
+     * @param array $conf
+     */
+    public function middlewares(array $conf): void
+    {
         if (DEBUG_MODE) {
-            $app->add(new WhoopsMiddleware($app));
+            $this->app->add(new WhoopsMiddleware($this->app));
         }
 
         $this->app->add(new JwtAuthentication([
@@ -87,7 +119,7 @@ class SlimApp implements WebApp
                         $logger->debug('Emitido: '.$event->getName(), $data);
                     }
                 } catch (\Exception $e) {
-                    $data['excpetion'] = $e->getMessage();
+                    $data['exception'] = $e->getMessage();
                     $data['file'] = $e->getFile() . ': '. $e->getLine();
 
                     $logger->error('Emitido: '.$event->getName(), $data);
@@ -96,31 +128,47 @@ class SlimApp implements WebApp
 
             return $response;
         });
-
-        $this->app->options('/{routes:.+}', function (ServerRequestInterface $request, ResponseInterface $response) {
-            return $response;
-        });
-
-        $this->app->add(function (ServerRequestInterface $request, ResponseInterface $response, $next) {
-            /** @var ResponseInterface $response */
-            $response = $next($request, $response);
-            return $response
-                ->withHeader('Access-Control-Allow-Origin', '*')
-                ->withHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, X-Requested-With, Content-Type, Accept, Origin, Authorization, Cache-Control')
-                ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH')
-                ->withHeader('Access-Control-Max-Age', '604800');
-        });
-
-        $this->app->get('/purge', function (ServerRequestInterface $request, ResponseInterface $response) {
-            /** @var CacheHelper $cache */
-            $cache = $this->get(CacheHelper::class);
-            $cache->purge();
-            return new Response(204);
-        });
-
-        $container->get(EloquentDatabaseProvider::class);
     }
 
+    /**
+     * @param CacheHelper $cacheHelper
+     */
+    private function loadRoutes(CacheHelper $cacheHelper)
+    {
+        $routes = $cacheHelper->remember('all_routes', function () {
+            $filename = ROOT_DIR.'/src/Application/Resources/routes.yml';
+            return yaml_parse_file($filename);
+        });
+
+        foreach ($routes as $controller => $actions) {
+            foreach ($actions as $action => $config) {
+                $this->app->map([$config['verb']], $config['route'], [$controller, $action]);
+            }
+        }
+    }
+
+    /**
+     * @param CacheHelper $cacheHelper
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     */
+    private function setListeners(CacheHelper $cacheHelper)
+    {
+        $eventDispatcher = $this->container->get('EventDispatcher');
+
+        $listeners = $cacheHelper->remember('all_listeners', function () {
+            $filename = ROOT_DIR.'/src/Application/Resources/listeners.yml';
+            return yaml_parse_file($filename);
+        });
+
+        foreach ($listeners as $class => $listener) {
+            $eventDispatcher->addListener($listener['event'], $this->container->get($class));
+        }
+    }
+
+    /**
+     * @throws \Exception
+     */
     public function run(): void
     {
         try {
